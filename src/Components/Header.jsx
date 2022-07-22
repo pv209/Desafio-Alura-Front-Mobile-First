616a98ad9ba54675a33e7e84d58ae1a13e8ea4d4
pv209 <pvtrybe209@gmail.com>
2022-07-22 16:11:00 -0300

    comecando a pagina de pets

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,13 +2,16 @@ import "../Pages/Home/home.css";
 import casa from "../Pages/Imagens/Casa.png";
 import forma1 from "../Pages/Imagens/Forma 1.png";
 import mensagem from "../Pages/Imagens/Mensagens.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div class="header">
-      <img src={casa} alt="casa" class="casa" />
-      <img src={forma1} alt="forma" class="forma" />
-      <img src={mensagem} alt="mensagem" class="mensagem" />
+      <Link to="/">
+        <img src={casa} alt="casa" class="casa" />
+        <img src={forma1} alt="forma" class="forma" />
+        <img src={mensagem} alt="mensagem" class="mensagem" />
+      </Link>
     </div>
   );
 }
--- src/Pages/Pets/Pets.jsx
+++ src/Pages/Pets/Pets.jsx
@@ -1,7 +1,35 @@
 import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
+import "./pets.css";
+import dunga from "../Imagens/Imagem Dunga.png";
+import mensagem from "../Imagens/iconemensagem.png"
 
-function Cadastro() {
-  return <Header />;
+function Pets() {
+  return (
+    <>
+      <Header />
+      <div className="main__pets">
+        <h5>
+          Olá! Veja os amigos
+          <br /> disponíveis para adoção!
+        </h5>
+        <div className="main__cards">
+          <div className="main__card">
+            <img src={dunga} alt="dunga" className="pets__img" />
+            <div className="cards__text">
+              <h4>Dunga</h4>
+              <p>2anos</p>
+              <p className="desc">Parte pequeno<br/>Calmo e educado</p>
+              <p className="local">Rio de Janeiro (RJ)</p>
+              <p className="resp"> <img src={mensagem} alt="mensagem" className="icone__mensagem" />  Falar com o responsavel</p>
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default Cadastro;
+export default Pets;
