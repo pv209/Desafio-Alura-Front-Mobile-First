dc203a74d8c99b259a7fa0285a1a2e61104dbec9
pv209 <pvtrybe209@gmail.com>
2022-07-27 09:55:26 -0300

    ajustes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,9 +9,11 @@ function Header() {
     <div class="header">
       <Link to="/">
         <img src={casa} alt="casa" class="casa" />
-        <img src={forma1} alt="forma" class="forma" />
-        <img src={mensagem} alt="mensagem" class="mensagem" />
       </Link>
+      <img src={mensagem} alt="mensagem" class="mensagem" />
+      <div class="forma1">
+        <img src={forma1} alt="forma" class="forma" />
+      </div>
     </div>
   );
 }
